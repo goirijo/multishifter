ba040f2b4232749a60d746147138d88a4b221803
John Goiri <jg.goiri@gmail.com>
2020-02-14 18:32:38 -0800

    Trivial changes

--- tests/trivial/trivial.cpp
+++ tests/trivial/trivial.cpp
@@ -5,6 +5,21 @@ TEST(CategoryTest, SpecificTest)
     ASSERT_EQ(0, 0);
 }
 
+TEST(OtherCategoryTest, SpecificTest)
+{
+    ASSERT_EQ(0, 0);
+}
+
+/* TEST(CategoryTest, PrintMessage) */
+/* { */
+/*     EXPECT_EQ(0, 0) << "I didn't actually expect this to be equal."; */
+/* } */
+
+/* TEST(CategoryTest, ExpectedFail) */
+/* { */
+/*     EXPECT_EQ(1, 0); */
+/* } */
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
